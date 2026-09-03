5000ed38719f3e7c44dedda44ebb2ee8fb682280
HARSHIT MISHRA <harshit.mishra925954@gmail.com>
2026-09-04 03:50:01 +0530

    Time: 0 ms (100%), Space: 47.7 MB (96.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,26 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int low=0; int high=nums.length-1;int res=-1;
-        while(low<=high){
-            int guess=(low+high)/2;
-            if(nums[guess]==target){
-                res=guess;
-                high=guess-1;
-            }else if(nums[guess]<target) low=guess+1;
-            else high=guess-1;
+        int left=0;
+        int right=nums.length-1;
+        int first=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                first=mid;
+                right=mid-1;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+            
+
         }
-        int low1=0; int high1=nums.length-1;
-        int res1=-1;
-        while(low1<=high1){
-            int guess=(low1+high1)/2;
-            if(nums[guess]==target){
-                res1=guess;
-                low1=guess+1;
-            }else if(nums[guess]<target) low1=guess+1;
-            else high1=guess-1;
-        }
-        if(res!=-1 && res1==-1) return new int[]{res,res};
-        else if(res!=-1 && res1!=-1) return new int[]{res,res1};
-        else return new int[]{-1,-1};
+         left=0; right=nums.length-1; int last=-1;
+         while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                last=mid;
+                left=mid+1;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+            
+         }
+         if(first!=-1&&last==-1) return new int[]{first,first};
+         if(first!=-1 && last!=-1) return new int[]{first,last};
+         return new int[]{-1,-1};
     }
 }
