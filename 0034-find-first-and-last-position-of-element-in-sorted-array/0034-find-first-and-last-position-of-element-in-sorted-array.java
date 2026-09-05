92ffe8d4cf8503a9b2d1f46729782597d885d355
HARSHIT MISHRA <harshit.mishra925954@gmail.com>
2026-09-06 04:02:47 +0530

    Time: 0 ms (100%), Space: 48.1 MB (51.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
         int first=-1;
-        while(left<=right){
-            int mid=(left+right)/2;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
             if(nums[mid]==target){
                 first=mid;
-                right=mid-1;
+                high=mid-1;
             }
             else if(nums[mid]<target){
-                left=mid+1;
+                low=mid+1;
+
             }
             else{
-                right=mid-1;
+                high=mid-1;
             }
-            
-
         }
-         left=0; right=nums.length-1; int last=-1;
-         while(left<=right){
-            int mid=(left+right)/2;
+        low=0;
+        high=nums.length-1;
+        int last=-1;
+         while(low<=high){
+            int mid=(low+high)/2;
             if(nums[mid]==target){
                 last=mid;
-                left=mid+1;
+                low=mid+1;
             }
             else if(nums[mid]<target){
-                left=mid+1;
+                low=mid+1;
+
             }
             else{
-                right=mid-1;
+                high=mid-1;
             }
-            
-         }
-         if(first!=-1&&last==-1) return new int[]{first,first};
-         if(first!=-1 && last!=-1) return new int[]{first,last};
-         return new int[]{-1,-1};
+        }
+        if(first!=-1&&last==-1) return new int[]{first,first};
+        else if(first!=-1&&last!=-1) return new int[]{first,last};
+        else return new int[]{-1,-1};
+        
+        
     }
 }
